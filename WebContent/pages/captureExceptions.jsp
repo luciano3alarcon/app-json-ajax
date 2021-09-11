fdb713a5e678e6f8f7880d55b6b65407578c5c0c
luciano3alarcon <lualarcon@gmail.com>
2021-09-11 17:41:44 +0200

    Überprüfung von Loggin generisch gebaut.

--- WebContent/pages/captureExceptions.jsp
+++ WebContent/pages/captureExceptions.jsp
@@ -24,6 +24,7 @@
 </body>
 
 <script>
+
 	function checkException() {
 		var printEingabe = $('#Eingabe').val();
 
--- src/filter/FilterAuthentifizierung.java
+++ src/filter/FilterAuthentifizierung.java
@@ -5,13 +5,19 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import nutzer.EingeloggteNutzer;
 
 //fängt die Zugänge zu anderen Seite ab, bevor es über Login läuft.
-@WebFilter(urlPatterns = { "/pages/systemZugang.jsp" })
+//Mit dem * werden alles Seite in dem Ordner Pages 
+@WebFilter(urlPatterns = { "/pages/*" })
 public class FilterAuthentifizierung implements Filter {
 
 //	Tut etwas, wenn die Applikation ausgefallen ist,
@@ -24,10 +30,27 @@ public class FilterAuthentifizierung implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 
+		HttpServletRequest  req = (HttpServletRequest) request;
+		HttpSession session = req.getSession(); 
+		
+		//Das ist für generische Authentifizierung
+		String urlZurAuthentifizieren = req.getServletPath(); 
+		
+		EingeloggteNutzer eingeloggteNutzer  =  (EingeloggteNutzer) session.getAttribute("username");
+		
+		//Bei der ersten Anmeldung kommt der username mit Wert null
+		if(eingeloggteNutzer == null && !urlZurAuthentifizieren.equalsIgnoreCase("/pages/ServletAuthentifizierung") ) {	
+			//
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/authentifizierung.jsp?url="+urlZurAuthentifizieren);
+			//Wenn der Nutzer noch nicht eingeloggt ist, wird er zu Login-Seite geschickt.
+			dispatcher.forward(request, response);
+			
+			return; // Bei nicht eingeloggtem Nutzer wird die Seite-Login zurückgeschickt.  
+		}
+		
+		
 		//Der Filter muss über den Aufruf chain.doFilter durchlaufen, sonst wird die Seite nicht angezeigt
 		chain.doFilter(request, response);
-		
-		System.out.println("Abfang"); 
 		
 	}
 
